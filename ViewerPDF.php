2f91042e116dba92e7d82539012ccbab971407ad
Wahyu Gugus Nurcahyo <wahyugnc@gmail.com>
2015-07-07 15:12:08 +0700

    Update ViewerPDF.php

--- ViewerPDF.php
+++ ViewerPDF.php
@@ -17,45 +17,43 @@ use yii\helpers\Html;
 
 class ViewerPDF extends Widget
 {
-
 	public $url;
-
 	public $options=[
-      	'sideBarOpen'=>false,
-      	'direction'=>'ltr',
-      	'buttons'=>[
-        	'sidebarToggle'=>true,
-        	'viewFind'=>true,
-        	'pageUp'=>true,
-        	'pageDown'=>true,        
-        	'zoomIn'=>true,
-        	'zoomOut'=>true,
-        	'scaleSelect'=>true,
-        	'presentationMode'=>true,
-        	'print'=>false,
-        	'openFile'=>false,
-        	'download'=>false,
-        	'viewBookmark'=>false,
-        ]
-    ];
+	      'sideBarOpen'=>false,
+	      'direction'=>'ltr',
+	      'buttons'=>[
+		'sidebarToggle'=>true,
+		'viewFind'=>true,
+		'pageUp'=>true,
+		'pageDown'=>true,        
+		'zoomIn'=>true,
+		'zoomOut'=>true,
+		'scaleSelect'=>true,
+		'presentationMode'=>true,
+		'print'=>false,
+		'openFile'=>false,
+		'download'=>false,
+		'viewBookmark'=>false,
+	]
+	];
 
 
-  	public function init()
-  	{
-    	if(!in_array($this->options['direction'],['rtl','ltr']))
+    public function init()
+    {
+  	if(!in_array($this->options['direction'],['rtl','ltr']))
       	$this->options['direction']='ltr';
       	parent::init();
-  	}
+    }
 
     public function run()
     {
-		Yii::setAlias('@pdfjs', '../../../../vendor/wahyugnc/yii2-pdfjs/assets');
-	    $baseUrl=Yii::getAlias('@pdfjs');  
+	Yii::setAlias('@pdfjs', '../../../../vendor/wahyugnc/yii2-pdfjs/assets');
+	$baseUrl=Yii::getAlias('@pdfjs');  
 
-	    return $this->render('_viewer',array(
-	      'baseUrl'=>$baseUrl,
-	      'url'=>$this->url,
-	      'options'=>$this->options,
-	    ));
+    	return $this->render('_viewer',array(
+      		'baseUrl'=>$baseUrl,
+		'url'=>$this->url,
+      		'options'=>$this->options,
+    	));
     }
 }
