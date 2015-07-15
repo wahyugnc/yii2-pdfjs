6afc9f791a3681267fcd18177e7dd674b3d15f22
Wahyu Gugus Nurcahyo <wahyugnc@gmail.com>
2015-07-15 07:13:33 +0700

    Update Fix

--- ViewerPDF.php
+++ ViewerPDF.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * @author Wahyu Gugus Nurcahyo <wahyugnc@gmail.com>
- * @link https://github.com/wahyugnc/yii2-pdfjs
- * @license http://www.opensource.org/licenses/bsd-license.php
- * @package https://packagist.org/packages/wahyugnc/yii2-pdfjs
- * @version 1.0.0
- */
 
 namespace wahyugnc\pdfjs;
 
@@ -17,43 +10,52 @@ use yii\helpers\Html;
 
 class ViewerPDF extends Widget
 {
-     public $url;
-     public $options=[
-	      'sideBarOpen'=>false,
-	      'direction'=>'ltr',
-	      'buttons'=>[
-		  'sidebarToggle'=>true,
-		  'viewFind'=>true,
-		  'pageUp'=>true,
-		  'pageDown'=>true,        
-		  'zoomIn'=>true,
-		  'zoomOut'=>true,
-		  'scaleSelect'=>true,
-		  'presentationMode'=>true,
-		  'print'=>false,
-		  'openFile'=>false,
-		  'download'=>false,
-		  'viewBookmark'=>false,
-	       ]
-     ];
+
+	public $url;
+
+	public $options=[
+      	'sideBarOpen'=>false,
+      	'direction'=>'ltr',
+      	'buttons'=>[
+        	'sidebarToggle'=>true,
+        	'viewFind'=>true,
+        	'pageUp'=>true,
+        	'pageDown'=>true,        
+        	'zoomIn'=>true,
+        	'zoomOut'=>true,
+        	'scaleSelect'=>true,
+        	'presentationMode'=>true,
+        	'print'=>false,
+        	'openFile'=>false,
+        	'download'=>false,
+        	'viewBookmark'=>false,
+        ]
+    ];
 
 
-    public function init()
-    {
-  	if(!in_array($this->options['direction'],['rtl','ltr']))
+  	public function init()
+  	{
+    	if(!in_array($this->options['direction'],['rtl','ltr']))
       	$this->options['direction']='ltr';
       	parent::init();
-    }
+  	}
 
     public function run()
     {
-	Yii::setAlias('@pdfjs', '../../../../vendor/wahyugnc/yii2-pdfjs/assets');
-	$baseUrl=Yii::getAlias('@pdfjs');  
+      $path=Yii::getAlias('@web');
+      $split_path=explode("/", $path);
+      if(in_array("frontend", $split_path)){
+          $pathassets=str_replace("frontend/web", "", $path);
+      }else{
+          $pathassets=str_replace("backend/web", "", $path);
+      }
+      Yii::setAlias('@pdfjs', $pathassets.'vendor/wahyugnc/yii2-pdfjs/assets');
+	    $baseUrl=Yii::getAlias('@pdfjs');  
 
-    	return $this->render('_viewer',array(
-      		'baseUrl'=>$baseUrl,
-		'url'=>$this->url,
-      		'options'=>$this->options,
-    	));
+	    return $this->render('_viewer',array(
+	      'baseUrl'=>$baseUrl,
+	      'url'=>$this->url,
+	      'options'=>$this->options,
+	    ));
     }
 }
